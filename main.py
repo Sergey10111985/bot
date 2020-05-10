faec734b2959cf2cc4b0e33875c73d15743ad1a9
Sergey10111985 <sirotkin.serega1985@yandex.ru>
2020-05-11 00:14:05 +0700

    New feachers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import telebot
+from telebot import types
 from bs4 import BeautifulSoup
 
 t_token = '1258725940:AAEd8aDB33v_6e30tWHa18jf1A1Mo4p7VdQ'
@@ -54,8 +55,23 @@ bot = telebot.TeleBot(t_token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Greetengs!!!\nEnter the country that you are "
-                          "interested in situation COVID-19")
+    markup = types.InlineKeyboardMarkup()
+    but1 = types.InlineKeyboardButton(text="Yes", callback_data="a")
+    but2 = types.InlineKeyboardButton(text="No", callback_data="b")
+    but3 = types.InlineKeyboardButton(text="Who did this sh*t", callback_data="c")
+    markup.row(but1, but2, but3)
+    bot.reply_to(message, "Greetengs!!!\nEnter the country that you are interested in situation COVID-19"
+                          "\nDid you understand?", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def test_callback(call):
+    if call.data == "a":
+        bot.send_message(call.message.chat.id, "Ok, then you can ask")
+    elif call.data == "b":
+        bot.send_message(call.message.chat.id, "Look up and re-read description")
+    elif call.data == "c":
+        bot.send_message(call.message.chat.id, "Sirotkin Sergey")
 
 
 @bot.message_handler()
