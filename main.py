93de329b8c263d70e94ac7ba8c069e5727bf7ca9
Sergey10111985 <sirotkin.serega1985@yandex.ru>
2020-05-11 16:55:16 +0700

    q

--- main.py
+++ main.py
@@ -37,15 +37,11 @@ def gether_data(mas):
             if mas[i] == '':
                 mas[i] = 'No Data'
         mes = ("Country: {0}\nTotal Cases: {1}\nNew Cases: "
-               "{2}\nTotal Deaths:{3}\nTotal Recovered: {4}\nActive Cases: {5}").format(mas[1],
-                                                                                        mas[2],
-                                                                                        mas[3],
-                                                                                        mas[4],
-                                                                                        mas[6],
-                                                                                        mas[7], )
+               "{2}\nTotal Deaths:{3}\nTotal Recovered: "
+               "{4}\nActive Cases: {5}").format(mas[1], mas[2], mas[3], mas[4], mas[6], mas[7], )
 
     else:
-        mes = "No such country in our base..."
+        mes = "My creator didn't teach me this word yet :("
 
     return mes
 
